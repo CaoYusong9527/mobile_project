4122ecbe1099dc1d30a4d97052108ddd9bffe7f3
cys <1195754621@qq.com>
2020-11-03 09:31:10 +0800

    配置了路由以及layout的基本布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import LayoutIndex from '@/views/layout'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/', component: LayoutIndex, children: [] }
   ]
 })
 
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <div></div>
+  <div class="layout-container">
+    <van-nav-bar>
+      <template #left>
+        <div class="logo"></div>
+      </template>
+      <template #right>
+        <van-button
+          @click="$router.push('/search')"
+          class="search-btn"
+          round
+          type="info"
+          size="small"
+          icon="search">
+          搜索
+        </van-button>
+      </template>
+    </van-nav-bar>
+
+    <!-- 二级路由 -->
+    <router-view></router-view>
+
+    <!-- tabBar -->
+    <van-tabbar>
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
@@ -8,6 +36,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.layout-container {
+  // logo区域
+  .logo {
+    background: url("~@/assets/logo.png") no-repeat;
+    background-size: cover;
+    width: 100px;
+    height: 30px;
+  }
+  // 搜索按钮
+  .search-btn {
+    background-color: #5babfb;
+    width: 100px;
+    font-size: 14px;
+    .van-icon {
+      color: #fff;
+    }
+  }
+}
 </style>
